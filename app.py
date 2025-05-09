0c32c7eae853b1e9887ed6a09a7c5df89075adee
Vidhu <totallyrealemail@oneofthese.yo>
2025-05-09 18:19:48 +0530

    feat: check with active session

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 
 st.balloons()
 st.title('Sanskriti Yatra')
+
+try:
+    session = get_active_session()
+    session.use_database("SANSKRITI_YATRA_DB")
+    session.use_schema("PUBLIC")
+except Exception as e:
+    st.error(f"Failed to set database context: {e}")
